66b8b2987ec9bcc5201ee4c31f9c5c7e39896d54
Bobby <rfgerace@gmail.com>
2018-02-15 10:48:42 -0500

    more work on UI

--- View.elm
+++ View.elm
@@ -6,22 +6,32 @@ import Html.Events exposing (onClick, onWithOptions)
 import Types exposing (..)
 import Array
 import Json.Decode as D
+import Char
 
 view : Model -> Html Msg
 view model = 
-        div [] [
-          span [] [mineDisplay model],
-          span [] [
-              button [onClick ResetGame] [buttonFace model.state]
-          ],
-          span [] [timer model.state],
-          table [] [
-            minefield model
-          ]
+    table [class "game-container"] [
+        tbody [] [
+            tr [class "game-header"] [
+                td [class "bomb-count-cell"] [mineDisplay model],
+                td [class "face-button-cell"] [
+                  button [onClick ResetGame, class "face-button"] [buttonFace model.state]
+                ],
+                td [class "timer-cell"] [timer model.state]
+            ],
+            tr [] [
+                td [class "field-container-cell", colspan 3] [
+                    minefield model
+                ]
+            ]
         ]
+    ]
                                                         
 minefield : Model -> Html Msg
-minefield model = fieldView model.field
+minefield model = 
+    table [class "field-table"] [
+        fieldView model.field
+    ]
 
 fieldView : Minefield -> Html Msg
 fieldView field = 
@@ -31,28 +41,59 @@ fieldView field =
 rowView : Int -> MineRow -> Html Msg
 rowView i row = 
         let arr = Array.indexedMap (cellView i) row
-        in tr [] (Array.toList arr)
+        in tr [class "field-row"] (Array.toList arr)
 
 cellView : Int -> Int -> Cell -> Html Msg
 cellView i j cell =
-        let content = case cell.contents of
-                        Bomb -> text "b"
-                        Number n -> n |> toString |> text
-                        _ -> text ""
-            classes = "mine" ++ case cell.state of
-                                  Clicked   -> " clicked"
-                                  Unclicked Flagged -> " flagged"
-                                  Unclicked Question -> " question"
-                                  Unclicked None     -> " unclicked"
-                                     
-        in td [onClick (CellClicked (i,j)), onRightClick (i, j)] [button [class classes] [content]]
+        let content = cellContents i j cell
+            classes = case (cell.contents, cell.state) of
+                          (Bomb, Clicked)   -> "field-cell clicked clicked-bomb"
+                          (_ , Clicked)     -> "field-cell clicked" 
+                          _                 -> "field-cell"
+        in td [class classes] [content]
+
+
+cellContents : Int -> Int -> Cell -> Html Msg
+cellContents i j cell = 
+    case (cell.state, cell.contents) of
+        (Unclicked flag, _) -> button 
+                                [ 
+                                  class "field-button",
+                                  onClick (CellClicked (i,j)), 
+                                  onRightClick (i, j)
+                                ] 
+                                [flagToStr flag |> text]
+        (Clicked, Bomb)     -> text "💣" 
+        (Clicked, Number n) -> span [class ("number-cell " ++ numClass n)] [n |> toString |> text]
+        _                   -> text ""
+
+numClass : Int -> String
+numClass n =
+    case n of
+        1 -> "one"
+        2 -> "two"
+        3 -> "three"
+        4 -> "four"
+        5 -> "five"
+        6 -> "six"
+        7 -> "seven"
+        8 -> "eight"
+        _ -> ""
+
+flagToStr : FlagState -> String
+flagToStr f =
+    case f of
+        Flagged  -> "🚩" 
+        Question -> "❔" 
+        None     -> Char.fromCode 160 |> String.fromChar
 
 buttonFace : GameState -> Html Msg
 buttonFace state = 
     case state of
-        Won _  -> text "B-)"
-        Lost _ -> text ":-("
-        _      -> text ":-)" 
+        --Won _  -> text "😯"
+        Won _  -> text "😎"
+        Lost _ -> text "💀"
+        _      -> text "🙂"
 
 timer : GameState -> Html Msg
 timer state =
